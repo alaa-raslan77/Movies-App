dc0e30372995a2f9c8f02ff52e981c3706cae6fa
AlaaRaslan <alaaraslan152@gmail.com>
2024-08-27 15:44:11 +0300

    first commit

---
+++ lib/colors_and_theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColors{
+
+  static  Color colorBlack =Color(0xff121312);
+  static  Color colorMidBlack =Color(0xff282A28);
+  static  Color colorOrange =Color(0xffF7B539);
+  static  Color colorGray =Color(0xff514F4F);
+  static  Color colorYellow =Color(0xffFFBB3B);
+}
